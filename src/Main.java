cade98b468b16603a5e4b8b54838def4ea681112
Dedurin <grem85krbr@mail,ru>
2024-02-01 12:24:54 +0400

    Lesson3

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+      CalcService service = new CalcService();
+
+        System.out.println();
+        System.out.println("5 / 6");
+        System.out.println(service.max(5,6));
+
+        System.out.println();
+       System.out.println("10/10");
+        System.out.println(service.max(10,10));
+
+        System.out.println();
+        System.out.println("-10 / -20 ");
+        System.out.println(service.max(-10,-20));
+
+
+
+    }
+}
